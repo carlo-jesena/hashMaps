60c36cd61ce2f31c970892f7a5a1f4a591fe5a87
Your Namecarlo <carlo.jesena@gmail.com>
2017-01-09 11:25:38 -0800

    finished the rest of the mthods

--- hashMapsRewrite.js
+++ hashMapsRewrite.js
@@ -17,17 +17,17 @@ HashMap._hashString = function(string) {
 }
 
 HashMap.prototype.get = function(key) {
-  var index = this._findSlot(key); 
+  var index = this._findSlot(key);
 
   if (this._slots[index] === undefined) {
     throw new Error ("Key error");
   }
 
-  return this._slots[index].value; 
+  return this._slots[index].value;
 }
 
 HashMap.prototype.set = function(key, value) {
-  var loadRatio = (this.length + this._deleted + 1) / this._capacity; 
+  var loadRatio = (this.length + this._deleted + 1) / this._capacity;
 
   if (loadRatio > HashMap.MAX_LOAD_RATIO) {
     this._resize(this._capacity * HashMap.SIZE_RATIO)
@@ -37,10 +37,48 @@ HashMap.prototype.set = function(key, value) {
 
   this._slots[index] = {
     key,
-    value, 
+    value,
     deleted: false
   }
 
-  this.length++; 
+  this.length++;
 }
 
+HashMap.prototype.remove = function(key) {
+  var index = this._findSlot(key);
+  var slot = this._slots[index];
+  if (slot === undefinded) {
+    throw new Error("Key error");
+  }
+  slot.deleted = true;
+  this.length--;
+  this._deleted++;
+}
+
+HashMap.prototype._findSlot = function(key) {
+  var hash = HashMap._hashString(key);
+  var start = hash % this._capacity;
+
+  for (var i=start; i<start + this._capacity; i++) {
+    var index = i % this._capacity;
+    var slot = this._slots[index];
+    if (slot === undefined || slot.key === key) {
+      return index;
+    }
+  }
+}
+
+HashMap.prototype._resize = function(size) {
+  var oldSlot = this._slots;
+  this._capacity = size;
+  this.length = 0;
+  this._deleted = 0;
+  this._slots = [];
+
+  for (var i =0; i<oldSlot.length; i++) {
+    var slot = oldSlot[i];
+    if (slot !== undefined) {
+      this.set(slot.key, slot.value);
+    }
+  }
+}
