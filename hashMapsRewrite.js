30d081731fe2ff59089757093632325200179600
Megan Smith <meg.eli.smi@gmail.com>
2017-01-09 11:11:04 -0800

    get set hashstring and intialize function rewritten

--- hashMapsRewrite.js
+++ hashMapsRewrite.js
@@ -15,3 +15,32 @@ HashMap._hashString = function(string) {
   }
   return hash >>> 0;
 }
+
+HashMap.prototype.get = function(key) {
+  var index = this._findSlot(key); 
+
+  if (this._slots[index] === undefined) {
+    throw new Error ("Key error");
+  }
+
+  return this._slots[index].value; 
+}
+
+HashMap.prototype.set = function(key, value) {
+  var loadRatio = (this.length + this._deleted + 1) / this._capacity; 
+
+  if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+    this._resize(this._capacity * HashMap.SIZE_RATIO)
+  }
+
+  var index = this._findSlot(key);
+
+  this._slots[index] = {
+    key,
+    value, 
+    deleted: false
+  }
+
+  this.length++; 
+}
+
